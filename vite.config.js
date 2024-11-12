8bf424ffcfc0a3d4c561fcefc414b9dcdb9629bd
Ea123 <nguyendohaiduy0204@gmail.com>
2024-11-12 20:42:22 +0700

    update login

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [
     react(),
     federation({
-      name: "shell",
+      name: "login",
       exposes: {
         "./Login": "./src/Login.jsx", 
       },
